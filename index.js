758e75a9346a34790b9aee4a3f32b3d82c0c2d0f
edel <edel.omondi@student.moringaschool.com>
2025-03-06 10:59:17 +0300

    updated 1 file

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const divide = (a, b) => a/b ;
+    console.log(divide(2000/100));
+    
+    
+const square = (a) => a**2;
+console.log(square(4));
+
+const add = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}
